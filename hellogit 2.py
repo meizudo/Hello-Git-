3f4462c610ae226b3bd6147b1a771633b62fe2b3
meizudo <griferia.extras_0j@icloud.com>
2024-03-25 16:39:21 +0100

    Comitamos nuevos cambios de 2 archivos a la vez

--- hellogit 2.py
+++ hellogit 2.py
@@ -1,3 +1,1 @@
-print("Hello git")
 print("Hello git v2")
-print("Hello git en que que añadimos esta linea de texto")
---
+++ hellogit.py
@@ -0,0 +1,2 @@
+print("He modificado todo. He eliminado todo el codigo y añadido esta linea")
+print("Esta es un alinea nueva de prueba")
